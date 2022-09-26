0366c47556c77803f91bec845475baedcfa0724f
ruslannnnnnnnn <unknownuser51472@gmail.com>
2022-09-26 22:52:09 +0500

    Скрыи кнопку Back если нельзя идти назад

--- KHIBATULLINTours/KHIBATULLINTours/MainWindow.xaml.cs
+++ KHIBATULLINTours/KHIBATULLINTours/MainWindow.xaml.cs
@@ -33,5 +33,17 @@ namespace KHIBATULLINTours
         {
             Manager.MainFrame.GoBack();
         }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnBack.Visibility = Visibility.Hidden;
+            }
+        }
     }
 }
